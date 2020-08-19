8dd55f6ba9fed7cf386111262d43b945c99a4037
justjavac <justjavac@gmail.com>
2020-08-19 23:02:01 +0800

    release v0.1.0

--- mod.ts
+++ mod.ts
@@ -1,15 +1,31 @@
-/**
- * Every exported symbol ideally should have a documentation line.
+/** Returns the path to the user's font directory.
  *
- * It is important that documentation is easily human readable,
- * but there is also a need to provide additional styling information to ensure
- * generated documentation is more rich text.
- * Therefore JSDoc should generally follow markdown markup to enrich the text.
- *
- * follow https://deno.land/std/style_guide.md
- *
- * @param foo - Description of non obvious parameter
+ * The returned value depends on the operating system and is either a string,
+ * containing a value from the following table, or `null`.
+ * 
+ * |Platform | Value                                                | Example                            |
+ * | ------- | ---------------------------------------------------- | ---------------------------------- |
+ * | Linux   | `$XDG_DATA_HOME`/fonts or `$HOME`/.local/share/fonts | /home/justjavac/.local/share/fonts |
+ * | macOS   | `$HOME/Library/Fonts`                                | /Users/justjavac/Library/Fonts     |
+ * | Windows | –                                                    | –                                  |
  */
-export default function starter(foo: string): string {
-  return foo;
+export default function fontDir(): string | null {
+  switch (Deno.build.os) {
+    case "linux": {
+      const xdg = Deno.env.get("XDG_DATA_HOME");
+      if (xdg) return `${xdg}/fonts`;
+
+      const home = Deno.env.get("HOME");
+      if (home) return `${home}/.local/share/fonts`;
+      break;
+    }
+
+    case "darwin": {
+      const home = Deno.env.get("HOME");
+      if (home) return `${home}/Library/Fonts`;
+      break;
+    }
+  }
+
+  return null;
 }
